4d872de2e184b0405bb7aa2948a1fdd4de6417e7
Riven <ther1ven@protonmail.com>
2025-04-17 15:17:31 +0300

    Initial commit

---
+++ example_frontend/frontend/src/apis/apiKey.js
@@ -0,0 +1,4 @@
+// cryptoProjectUser
+export const APIKey = 'Lg6TGbdNQBTq3IMNsQ9c5dCFEUpgXQS5IG5o7RZ5';
+
+
---
+++ example_frontend/frontend/src/apis/cryptoPortfolio.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+// cryptoProjectUser
+const instance = axios.create({
+    baseURL: 'http://localhost/api'
+});
+export default instance;
+
